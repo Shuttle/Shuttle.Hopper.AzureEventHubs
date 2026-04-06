bc5617c188725eb5c6cfa8b7e05ba6691a53326e
Eben <me@ebenroux.co.za>
2026-04-06 07:55:45 +0200

    Removed transaction scope.

--- Shuttle.Hopper.AzureEventHubs.Tests/EventHubInboxFixture.cs
+++ Shuttle.Hopper.AzureEventHubs.Tests/EventHubInboxFixture.cs
@@ -5,19 +5,16 @@ namespace Shuttle.Hopper.AzureEventHubs.Tests;
 
 public class EventHubInboxFixture : InboxFixture
 {
-    [TestCase(true, true)]
-    [TestCase(true, false)]
-    [TestCase(false, true)]
-    [TestCase(false, false)]
-    public async Task Should_be_able_handle_errors_async(bool hasErrorQueue, bool isTransactionalEndpoint)
+    [TestCase(true)]
+    [TestCase(false)]
+    public async Task Should_be_able_handle_errors_async(bool hasErrorQueue)
     {
-        await TestInboxErrorAsync(AzureEventHubsConfiguration.GetServiceCollection(), "azureeh://azure/{0}", hasErrorQueue, isTransactionalEndpoint, TimeSpan.FromMinutes(5));
+        await TestInboxErrorAsync(AzureEventHubsConfiguration.GetServiceCollection(), "azureeh://azure/{0}", hasErrorQueue, TimeSpan.FromMinutes(1));
     }
 
-    [TestCase(100, true)]
-    [TestCase(100, false)]
-    public async Task Should_be_able_to_process_queue_timeously_async(int count, bool isTransactionalEndpoint)
+    [Test]
+    public async Task Should_be_able_to_process_queue_timeously_async()
     {
-        await TestInboxThroughputAsync(AzureEventHubsConfiguration.GetServiceCollection(), "azureeh://azure/{0}", 2000, count, 1, isTransactionalEndpoint);
+        await TestInboxThroughputAsync(AzureEventHubsConfiguration.GetServiceCollection(), "azureeh://azure/{0}", 2000, 5, TimeSpan.FromMinutes(2));
     }
 }
